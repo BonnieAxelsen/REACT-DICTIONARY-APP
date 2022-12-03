3ce04f06aba588e1685b851a7d394fc8e5984229
BonnieAxelsen <104758739+BonnieAxelsen@users.noreply.github.com>
2022-12-03 15:30:26 +0100

    Edited and added link to footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "./styles/Footer.css";
 
 export default function Footer() {
   return (
     <footer>
-      <p> Coded and designed by Bonnie Axelsen, hosted on Netlify </p>
+      <p>
+        {" "}
+        Coded and designed by <br />
+        <a
+          href="https://github.com/BonnieAxelsen/REACT-DICTIONARY-APP"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Bonnie Axelsen
+        </a>
+        , hosted on Netlify
+      </p>
     </footer>
   );
 }
